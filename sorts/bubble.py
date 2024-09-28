aaaf25f1551d0945e6fe6661fe8af37d78498883
erfan <erfan.moqadasii@gmail.com>
2024-09-28 18:46:59 +0330

    bubble sort

---
+++ sorts/bubble.py
@@ -0,0 +1,15 @@
+
+def bubbleSort(array):
+    n = len(array)
+    for i in range(0, len(array)):
+        flag = False
+        for j in range(0, n - i - 1):
+            if array[j] >= array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                flag = True
+        if not flag:
+            break
+
+array = [10, 9, 8, 7, 1, 2, 3, 4]
+bubbleSort(array)
+print(array)
