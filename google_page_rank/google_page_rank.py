d9f30072a36d76d29943e4837b19897b9e8b12ff
erfan <erfan.moqadasii@gmail.com>
2024-10-04 22:42:09 +0330

    init google page rank algorythm

---
+++ google_page_rank/google_page_rank.py
@@ -0,0 +1,2 @@
+# Let's asumme that in ninty percent of the cases, the page rank of a page is equal to the page rank of its links.
+# And in ten percent of the cases, the page rank of a page is equal to the page rank of the page itself.
