677e9be7821d6377fe5a53b948a5c4e11d736f1d
erfan <erfan.moqadasii@gmail.com>
2024-10-10 17:22:02 +0330

    page rank operation on a n degree graphs

--- google_page_rank/google_page_rank.py
+++ google_page_rank/google_page_rank.py
@@ -1,2 +1,18 @@
 # Let's asumme that in ninty percent of the cases, the page rank of a page is equal to the page rank of its links.
 # And in ten percent of the cases, the page rank of a page is equal to the page rank of the page itself.
+
+from . import stdio
+
+n = stdio.readInt()
+link_conuts = [[0] * n for i in range(n)]
+out_degrees = [0] * n
+
+while not stdio.isEmpty():
+    i, j = stdio.readInt(), stdio.readInt()
+    out_degrees[i] += 1
+    link_conuts[i][j] += 1
+
+for i in range(n):
+    for j in range(n):
+        p = 0.9 * (link_conuts[i][j] / out_degrees[i]) + (0.1 / n)
+
---
+++ google_page_rank/readme.txt
@@ -0,0 +1,2 @@
+example command (power shell):
+Get-Content medium.txt | python transition.py | python randomsurfer.py 1000000 
